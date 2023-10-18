62cc9ca7bf3acb64fdc7999cdf829c61beca8eba
user <basalingappa.itagi@inqude.com>
2023-10-18 16:57:07 +0530

    implementing nav bar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,127 @@
+import React, { useContext } from "react";
+import "./NavBar.css";
+import {
+  Link,
+  Button,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller,
+} from "react-scroll";
+import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
+import { ThemeContext } from "../../context/ThemeContext";
+import Fade from "react-reveal/Fade";
+import Zoom from "react-reveal/Zoom";
+import Profile from "../../assets/profile.jpg";
+
+const NavBar = () => {
+  const [theme, setTheme] = useContext(ThemeContext);
+  return (
+    <header>
+      <nav className="navbarr">
+        <img src={Profile} alt="profile" />
+        <div className="menu">
+          <AiOutlineMenu />
+          <Link
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            Contact
+          </Link>
+          <Link
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Contact
+          </Link>
+        </div>
+        <div className="nav-bar">
+          <Link
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Contact
+          </Link>
+
+          <Link
+            activeClass="active"
+            to="project"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Projects
+          </Link>
+          <Link
+            activeClass="active"
+            to="techstack"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Tech stack
+          </Link>
+          <Link
+            activeClass="active"
+            to="experiance"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Experience
+          </Link>
+
+          <Link
+            activeClass="active"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            About
+          </Link>
+          <Link
+            activeClass="active"
+            to="home"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Home
+          </Link>
+          <div
+            onClick={() => {
+              setTheme((prev) => (prev === "light" ? "dark" : "light"));
+            }}
+          >
+            {theme === "light" ? (
+              <BsFillMoonStarsFill size={30} color="black" />
+            ) : (
+              <BsFillSunFill size={30} color="black" />
+            )}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
